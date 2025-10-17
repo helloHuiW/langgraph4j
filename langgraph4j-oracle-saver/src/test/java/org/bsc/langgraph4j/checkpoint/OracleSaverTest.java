f1a5ec0dffb611515823a5d738500e71310191dc
Fernanda Meheust <fernanda.meheust@oracle.com>
2025-10-17 22:55:36 +0200

    Format and name

--- langgraph4j-oracle-saver/src/test/java/org/bsc/langgraph4j/checkpoint/OracleSaverTest.java
+++ langgraph4j-oracle-saver/src/test/java/org/bsc/langgraph4j/checkpoint/OracleSaverTest.java
@@ -26,180 +26,177 @@ import org.testcontainers.utility.MountableFile;
 
 public class OracleSaverTest {
 
-  protected static final String ORACLE_IMAGE_NAME = "gvenzl/oracle-free:23.7-slim-faststart";
-  protected static final OracleDataSource DATA_SOURCE;
-  protected static final OracleDataSource SYSDBA_DATA_SOURCE;
+    protected static final String ORACLE_IMAGE_NAME = "gvenzl/oracle-free:23.7-slim-faststart";
+    protected static final OracleDataSource DATA_SOURCE;
+    protected static final OracleDataSource SYSDBA_DATA_SOURCE;
 
-  static {
+    static {
 
-    try {
-      DATA_SOURCE = new oracle.jdbc.datasource.impl.OracleDataSource();
-      SYSDBA_DATA_SOURCE = new oracle.jdbc.datasource.impl.OracleDataSource();
-      String urlFromEnv = System.getenv("ORACLE_JDBC_URL");
+        try {
+            DATA_SOURCE = new oracle.jdbc.datasource.impl.OracleDataSource();
+            SYSDBA_DATA_SOURCE = new oracle.jdbc.datasource.impl.OracleDataSource();
+            String urlFromEnv = System.getenv("ORACLE_JDBC_URL");
 
-      if (urlFromEnv == null) {
-        // The Ryuk component is relied upon to stop this container.
-        OracleContainer oracleContainer = new OracleContainer(ORACLE_IMAGE_NAME)
-            .withStartupTimeout(Duration.ofSeconds(600))
-            .withConnectTimeoutSeconds(600)
-            .withDatabaseName("pdb1")
-            .withUsername("testuser")
-            .withPassword("testpwd");
-        oracleContainer.start();
+            if (urlFromEnv == null) {
+                // The Ryuk component is relied upon to stop this container.
+                OracleContainer oracleContainer = new OracleContainer(ORACLE_IMAGE_NAME)
+                        .withStartupTimeout(Duration.ofSeconds(600))
+                        .withConnectTimeoutSeconds(600)
+                        .withDatabaseName("pdb1")
+                        .withUsername("testuser")
+                        .withPassword("testpwd");
+                oracleContainer.start();
 
-        initDataSource(
-            DATA_SOURCE,
-            oracleContainer.getJdbcUrl(),
-            oracleContainer.getUsername(),
-            oracleContainer.getPassword());
-        initDataSource(SYSDBA_DATA_SOURCE, oracleContainer.getJdbcUrl(), "sys", oracleContainer.getPassword());
-      } else {
-        initDataSource(
-            DATA_SOURCE,
-            urlFromEnv,
-            System.getenv("ORACLE_JDBC_USER"),
-            System.getenv("ORACLE_JDBC_PASSWORD"));
-        initDataSource(
-            SYSDBA_DATA_SOURCE,
-            urlFromEnv,
-            System.getenv("ORACLE_JDBC_USER"),
-            System.getenv("ORACLE_JDBC_PASSWORD"));
-      }
-      SYSDBA_DATA_SOURCE.setConnectionProperty(OracleConnection.CONNECTION_PROPERTY_INTERNAL_LOGON, "SYSDBA");
+                initDataSource(
+                        DATA_SOURCE,
+                        oracleContainer.getJdbcUrl(),
+                        oracleContainer.getUsername(),
+                        oracleContainer.getPassword());
+                initDataSource(SYSDBA_DATA_SOURCE, oracleContainer.getJdbcUrl(), "sys", oracleContainer.getPassword());
+            } else {
+                initDataSource(
+                        DATA_SOURCE,
+                        urlFromEnv,
+                        System.getenv("ORACLE_JDBC_USER"),
+                        System.getenv("ORACLE_JDBC_PASSWORD"));
+                initDataSource(
+                        SYSDBA_DATA_SOURCE,
+                        urlFromEnv,
+                        System.getenv("ORACLE_JDBC_USER"),
+                        System.getenv("ORACLE_JDBC_PASSWORD"));
+            }
+            SYSDBA_DATA_SOURCE.setConnectionProperty(OracleConnection.CONNECTION_PROPERTY_INTERNAL_LOGON, "SYSDBA");
 
-    } catch (SQLException sqlException) {
-      throw new AssertionError(sqlException);
+        } catch (SQLException sqlException) {
+            throw new AssertionError(sqlException);
+        }
     }
-  }
 
-  static void initDataSource(OracleDataSource dataSource, String url, String username, String password) throws SQLException {
-    dataSource.setURL(url + "?oracle.jdbc.provider.json=jackson-json-provider");
-    dataSource.setUser(username);
-    dataSource.setPassword(password);
-  }
+    static void initDataSource(OracleDataSource dataSource, String url, String username, String password)
+            throws SQLException {
+        dataSource.setURL(url + "?oracle.jdbc.provider.json=jackson-json-provider");
+        dataSource.setUser(username);
+        dataSource.setPassword(password);
+    }
 
-  @Test
-  public void testCheckpointWithReleasedThread() throws Exception {
+    @Test
+    public void testCheckpointWithReleasedThread() throws Exception {
 
-    var saver = OracleSaver.builder()
-        .dataSource(DATA_SOURCE)
-        .build();
+        var saver = OracleSaver.builder()
+                .dataSource(DATA_SOURCE)
+                .build();
 
-    NodeAction<AgentState> agent_1 = state -> {
-      return Map.of("agent_1:prop1", "agent_1:test");
-    };
+        NodeAction<AgentState> agent_1 = state -> {
+            return Map.of("agent_1:prop1", "agent_1:test");
+        };
 
-    var graph = new StateGraph<>(AgentState::new)
-        .addNode("agent_1", node_async( agent_1 ))
-        .addEdge( START,"agent_1")
-        .addEdge( "agent_1",  END)
-        ;
+        var graph = new StateGraph<>(AgentState::new)
+                .addNode("agent_1", node_async(agent_1))
+                .addEdge(START, "agent_1")
+                .addEdge("agent_1", END);
 
-    var compileConfig = CompileConfig.builder()
-        .checkpointSaver(saver)
-        .releaseThread(true)
-        .build();
+        var compileConfig = CompileConfig.builder()
+                .checkpointSaver(saver)
+                .releaseThread(true)
+                .build();
 
-    var runnableConfig = RunnableConfig.builder()
-        .build();
-    var workflow = graph.compile( compileConfig );
+        var runnableConfig = RunnableConfig.builder()
+                .build();
+        var workflow = graph.compile(compileConfig);
 
-    Map<String, Object> inputs = Map.of( "input", "test1");
+        Map<String, Object> inputs = Map.of("input", "test1");
 
-    var result = workflow.invoke( inputs, runnableConfig );
+        var result = workflow.invoke(inputs, runnableConfig);
 
-    assertTrue( result.isPresent() );
+        assertTrue(result.isPresent());
 
-    var history = workflow.getStateHistory( runnableConfig );
+        var history = workflow.getStateHistory(runnableConfig);
 
-    assertTrue( history.isEmpty() );
+        assertTrue(history.isEmpty());
 
-  }
+    }
 
-  @Test
-  public void testCheckpointWithNotReleasedThread() throws Exception {
-    var saver = OracleSaver.builder()
-        .createOption(CreateOption.CREATE_OR_REPLACE)
-        .dataSource(DATA_SOURCE)
-        .build();
+    @Test
+    public void testCheckpointWithNotReleasedThread() throws Exception {
+        var saver = OracleSaver.builder()
+                .createOption(CreateOption.CREATE_OR_REPLACE)
+                .dataSource(DATA_SOURCE)
+                .build();
 
+        NodeAction<AgentState> agent_1 = state -> {
+            return Map.of("agent_1:prop1", "agent_1:test");
+        };
 
-    NodeAction<AgentState> agent_1 = state -> {
-      return Map.of("agent_1:prop1", "agent_1:test");
-    };
+        var graph = new StateGraph<>(AgentState::new)
+                .addNode("agent_1", node_async(agent_1))
+                .addEdge(START, "agent_1")
+                .addEdge("agent_1", END);
 
-    var graph = new StateGraph<>(AgentState::new)
-        .addNode("agent_1", node_async( agent_1 ))
-        .addEdge( START,"agent_1")
-        .addEdge( "agent_1",  END)
-        ;
+        var compileConfig = CompileConfig.builder()
+                .checkpointSaver(saver)
+                .releaseThread(false)
+                .build();
 
-    var compileConfig = CompileConfig.builder()
-        .checkpointSaver(saver)
-        .releaseThread(false)
-        .build();
+        var runnableConfig = RunnableConfig.builder().build();
+        var workflow = graph.compile(compileConfig);
 
-    var runnableConfig = RunnableConfig.builder().build();
-    var workflow = graph.compile( compileConfig );
+        Map<String, Object> inputs = Map.of("input", "test1");
 
-    Map<String, Object> inputs = Map.of( "input", "test1");
+        var result = workflow.invoke(inputs, runnableConfig);
 
-    var result = workflow.invoke( inputs, runnableConfig );
+        assertTrue(result.isPresent());
 
-    assertTrue( result.isPresent() );
+        var history = workflow.getStateHistory(runnableConfig);
 
-    var history = workflow.getStateHistory( runnableConfig );
+        assertFalse(history.isEmpty());
+        assertEquals(2, history.size());
 
-    assertFalse( history.isEmpty() );
-    assertEquals( 2, history.size() );
+        var lastSnapshot = workflow.lastStateOf(runnableConfig);
 
-    var lastSnapshot = workflow.lastStateOf( runnableConfig );
+        assertTrue(lastSnapshot.isPresent());
+        assertEquals("agent_1", lastSnapshot.get().node());
+        assertEquals(END, lastSnapshot.get().next());
 
-    assertTrue( lastSnapshot.isPresent() );
-    assertEquals( "agent_1", lastSnapshot.get().node() );
-    assertEquals( END, lastSnapshot.get().next() );
+        // UPDATE STATE
+        final var updatedConfig = workflow.updateState(lastSnapshot.get().config(), Map.of("update", "update test"));
 
-    // UPDATE STATE
-    final var updatedConfig = workflow.updateState( lastSnapshot.get().config(), Map.of( "update", "update test") );
+        var updatedSnapshot = workflow.stateOf(updatedConfig);
+        assertTrue(updatedSnapshot.isPresent());
+        assertEquals("agent_1", updatedSnapshot.get().node());
+        assertTrue(updatedSnapshot.get().state().value("update").isPresent());
+        assertEquals("update test", updatedSnapshot.get().state().value("update").get());
+        assertEquals(END, lastSnapshot.get().next());
 
-    var updatedSnapshot = workflow.stateOf( updatedConfig );
-    assertTrue( updatedSnapshot.isPresent() );
-    assertEquals( "agent_1", updatedSnapshot.get().node() );
-    assertTrue( updatedSnapshot.get().state().value("update").isPresent() );
-    assertEquals( "update test", updatedSnapshot.get().state().value("update").get() );
-    assertEquals( END, lastSnapshot.get().next() );
+        // test checkpoints reloading from database
+        saver = OracleSaver.builder()
+                .dataSource(DATA_SOURCE)
+                .build();
 
-    // test checkpoints reloading from database
-    saver = OracleSaver.builder()
-        .dataSource(DATA_SOURCE)
-        .build();
+        compileConfig = CompileConfig.builder()
+                .checkpointSaver(saver)
+                .releaseThread(false)
+                .build();
 
-    compileConfig = CompileConfig.builder()
-        .checkpointSaver(saver)
-        .releaseThread(false)
-        .build();
+        runnableConfig = RunnableConfig.builder().build();
+        workflow = graph.compile(compileConfig);
 
-    runnableConfig = RunnableConfig.builder().build();
-    workflow = graph.compile( compileConfig );
+        history = workflow.getStateHistory(runnableConfig);
 
-    history = workflow.getStateHistory( runnableConfig );
+        assertFalse(history.isEmpty());
+        assertEquals(2, history.size());
 
-    assertFalse( history.isEmpty() );
-    assertEquals( 2, history.size() );
+        lastSnapshot = workflow.stateOf(updatedConfig);
+        // lastSnapshot = workflow.lastStateOf( runnableConfig );
 
-    lastSnapshot = workflow.stateOf(updatedConfig);
-    // lastSnapshot = workflow.lastStateOf( runnableConfig );
+        assertTrue(lastSnapshot.isPresent());
+        assertEquals("agent_1", lastSnapshot.get().node());
+        assertEquals(END, lastSnapshot.get().next());
+        assertTrue(lastSnapshot.get().state().value("update").isPresent());
+        assertEquals("update test", lastSnapshot.get().state().value("update").get());
+        assertEquals(END, lastSnapshot.get().next());
 
-    assertTrue( lastSnapshot.isPresent() );
-    assertEquals( "agent_1", lastSnapshot.get().node() );
-    assertEquals( END, lastSnapshot.get().next() );
-    assertTrue( lastSnapshot.get().state().value("update").isPresent() );
-    assertEquals( "update test", lastSnapshot.get().state().value("update").get() );
-    assertEquals( END, lastSnapshot.get().next() );
+        saver.release(runnableConfig);
 
-
-    saver.release( runnableConfig );
-
-  }
+    }
 
 }
